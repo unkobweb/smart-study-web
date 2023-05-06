80d35bbde9fd344ad5a2066907aab1d25430ea4f
LeoneLal <leone.lalloue@hotmail.fr>
2023-05-06 17:03:59 +0200

    feat(login):add validation rules

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -3,52 +3,89 @@
     <div class="login-container d-flex flex-column align-center justify-center">
       <h1>Connexion</h1>
       <div class="w-100 d-flex flex-column justify-center mb-4">
-        <v-text-field id="email-input" variant="outlined" prepend-inner-icon="at-outline" label="Email" v-model="email"></v-text-field>
-        <v-text-field id="password-input" variant="outlined" prepend-inner-icon="lock-outline" label="Password" v-model="password"></v-text-field>
+        <v-text-field
+          id="email-input"
+          variant="outlined"
+          prepend-inner-icon="at-outline"          
+          :rules="[emailRules.required]"
+          validate-on="blur"
+          label="Email"
+          v-model="email"
+        ></v-text-field>
+        <v-text-field
+          id="password-input"
+          variant="outlined"
+          prepend-inner-icon="lock-outline"
+          :append-inner-icon="showPassword ? 'eye-off-outline' : 'eye-outline'"
+          :type="showPassword ? 'text' : 'password'"
+          :rules="[passwordRules.required]"
+          validate-on="blur"
+          label="Password"
+          v-model="password"
+          @click:append="showPassword = !showPassword"
+        ></v-text-field>
         <v-btn variant="tonal" @click="signIn">SE CONNECTER</v-btn>
       </div>
       <v-btn-group class="d-flex flex-row">
-        <v-btn id="google-login-btn" prepend-icon="google" variant="tonal" @click="logViaGoogle" class="mr-2">Google</v-btn>
-        <v-btn id="linkedin-login-btn" prepend-icon="linkedin" variant="tonal" @click="logViaLinkedin">Linkedin</v-btn>
+        <v-btn
+          id="google-login-btn"
+          prepend-icon="google"
+          variant="tonal"
+          @click="logViaGoogle"
+          class="mr-2"
+          >Google</v-btn
+        >
+        <v-btn
+          id="linkedin-login-btn"
+          prepend-icon="linkedin"
+          variant="tonal"
+          @click="logViaLinkedin"
+          >Linkedin</v-btn
+        >
       </v-btn-group>
-      <p class="mt-4">Pas encore de compte ? <NuxtLink to="/signup">S'inscrire</NuxtLink></p>
+      <p class="mt-4">
+        Pas encore de compte ? <NuxtLink to="/signup">S'inscrire</NuxtLink>
+      </p>
     </div>
   </div>
 </template>
 
 <script setup>
-const {$event} = useNuxtApp()
+const { $event } = useNuxtApp();
 
-const email = ref("")
-const password = ref("")
+const email = ref("");
+const password = ref("");
+const showPassword = ref(false);
+
+const emailRules = ref({
+  required: value => !!value || "L'email est requis"
+});
+const passwordRules = ref({
+  required: (value) => !!value || "Le mot de passe est requis"
+});
 
 async function signIn() {
-  $event.$emit('show-snackbar', {
-    message: 'Connexion en cours...',
-    loading: true
-  })
-  return
-  if(email.value == "" ) {
-    alert('Veillez renseigner une adresse email dans la zone')
-  }
-  if(password.value == "") {
-    alert('Veillez renseigner une adresse email dans la zone')
-  }
-  await useApiFetch('/auth/login', {
-    method: 'POST',
+  $event.$emit("show-snackbar", {
+    message: "Connexion en cours...",
+    loading: true,
+  });
+  return;
+
+  await useApiFetch("/auth/login", {
+    method: "POST",
     body: {
       email: email.value,
-      password: password.value
-    }
-  })
+      password: password.value,
+    },
+  });
 }
 
 function logViaGoogle() {
-  window.location.href = "http://localhost:8080/auth/google"
+  window.location.href = "http://localhost:8080/auth/google";
 }
 
 function logViaLinkedin() {
-  window.location.href = "http://localhost:8080/auth/linkedin"
+  window.location.href = "http://localhost:8080/auth/linkedin";
 }
 </script>
 
