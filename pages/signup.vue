88fc4f530bd8b4bb0c89f8e61b499e6853fc9a65
LeoneLal <leone.lalloue@hotmail.fr>
2023-05-06 17:05:02 +0200

    feat(signup):add sign up page with validation rules on inputs

---
+++ pages/signup.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="w-100 h-screen d-flex flex-column align-center justify-center">
+    <div
+      class="signup-container d-flex flex-column align-center justify-center"
+    >
+      <h1>Inscription</h1>
+      <div class="w-100 d-flex flex-column justify-center mb-4">
+        <v-text-field
+          id="email-input"
+          variant="outlined"
+          prepend-inner-icon="at-outline"
+          label="Email"
+          v-model="email"
+          :rules="[emailRules.required, emailRules.reel]"
+          validate-on="blur"
+        ></v-text-field>
+
+        <v-text-field
+          id="password-input"
+          variant="outlined"
+          prepend-inner-icon="lock-outline"
+          :append-inner-icon="showPassword ? 'eye-off-outline' : 'eye-outline'"
+          :type="showPassword ? 'text' : 'password'"
+          :rules="[passwordRules.required, passwordRules.length]"
+          validate-on="blur"
+          label="Password"
+          v-model="password"
+          @click:append="showPassword = !showPassword"
+        ></v-text-field>
+        <v-text-field
+          id="password-input"
+          variant="outlined"
+          prepend-inner-icon="lock-outline"
+          :append-inner-icon="showPassword ? 'eye-off-outline' : 'eye-outline'"
+          :type="showPassword ? 'text' : 'password'"
+          :rules="[samePassword.rule]"
+          validate-on-blur
+          label="Confirm Password"
+          v-model="confirmedPassword"
+          @click:append="showPassword = !showPassword"
+        ></v-text-field>
+        <v-btn variant="tonal" @click="signUp">S'INSCRIRE</v-btn>
+      </div>
+      <p class="mt-4">
+        Déjà un compte ? <NuxtLink to="/login">Se connecter</NuxtLink>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { $event } = useNuxtApp();
+
+const email = ref("");
+const password = ref("");
+const confirmedPassword = ref("");
+const showPassword = ref(false);
+const emailRules = ref({
+  required: value => !!value || "L'email est requis",
+  reel: value =>
+    /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value) || "L'email n'est pas valide",
+});
+const passwordRules = ref({
+  required: (value) => !!value || "Le mot de passe est requis",
+  length: (value) =>
+    (value && value.length >= 8) ||
+    "Le mot de passe doit contenir au moins 8 caractères",
+});
+
+const samePassword = ref({
+    rule: value => password.value === value || 'Les deux mots de passe doivent être identiques'
+})
+
+async function signUp() {
+  console.log('Sign Up')
+}
+</script>
+
+<style lang="scss" scoped>
+.signup-container {
+  width: 100%;
+  max-width: 350px;
+  padding: 20px;
+}
+</style>
