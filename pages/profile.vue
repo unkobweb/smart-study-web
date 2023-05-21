1d3707e924786d0bf374f3287c2d3979c075ecf2
sieg.alexandre@gmail.com <sieg.alexandre@gmail.com>
2023-05-21 16:23:08 +0200

    feat(profile): improve profile page, and add query city

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,19 +1,65 @@
 <template>
-  <div class="pa-5">
-    <v-row>
-      <v-col cols="12" sm="8" md="8" lg="8" xl="8">
-
-      </v-col>
-      <v-col cols="12" sm="4" md="4" lg="4" xl="4">
-        <v-alert
-          :icon="user.enabled2Fa ? 'unlock-outline' : 'lock-outline'" 
-          :title="user.enabled2Fa ? 'Double authentification activée' : 'Double authentification'" 
-          :text="alertMsg"
-          :type="user.enabled2Fa ? 'success' : 'warning'"
-        ></v-alert>
-      </v-col>
-    </v-row>
-    {{ user || "pas user" }}
+  <div class="w-100 d-flex justify-center">
+    <div class="mw-40 mt-8">
+      <h2 class="mb-4">Votre profil</h2>
+      <v-row>
+        <v-col cols="12" sm="6" md="6" lg="6" xl="6">
+          <UserProfileForm />
+        </v-col>
+        <v-col cols="12" sm="6" md="6" lg="6" xl="6">
+          <v-alert 
+            :title="user.enabled2Fa ? 'Double authentification activée' : 'Double authentification'" 
+            :text="alertMsg"
+            :type="user.enabled2Fa ? 'success' : 'warning'"
+            @click="showSetup2Fa = true"
+          >
+            <template v-slot:prepend>
+              <v-icon fill="white">{{ user.enabled2Fa ? 'lock-outline' : 'unlock-outline' }}</v-icon>
+            </template>
+            <v-dialog v-model="showSetup2Fa" max-width="800">
+              <MfaSetup @close="showSetup2Fa = false" />
+            </v-dialog>
+          </v-alert>
+          <h3 class="mt-6">Statistiques</h3>
+          <v-card class="mt-4">
+            <v-card-text>
+              <v-row>
+                <v-col cols="12" sm="10" md="6" lg="10" xl="10">
+                  <p>Nombre de cours possédés</p>
+                </v-col>
+                <v-col cols="12" sm="2" md="2" lg="2" xl="2">
+                  <p>0</p>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="12" sm="10" md="10" lg="10" xl="10">
+                  <p>Nombre de cours terminés</p>
+                </v-col>
+                <v-col cols="12" sm="2" md="2" lg="2" xl="2">
+                  <p>0</p>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="12" sm="10" md="10" lg="10" xl="10">
+                  <p>Nombre de cours en cours</p>
+                </v-col>
+                <v-col cols="12" sm="2" md="2" lg="2" xl="2">
+                  <p>0</p>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="12" sm="10" md="10" lg="10" xl="10">
+                  <p>Nombre de cours à faire</p>
+                </v-col>
+                <v-col cols="12" sm="2" md="2" lg="2" xl="2">
+                  <p>0</p>
+                </v-col>
+              </v-row>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
   </div>
 </template>
 
@@ -24,13 +70,15 @@ definePageMeta({
   middleware: ["auth"]
 })
 
+const showSetup2Fa = ref(false);
+
 const { user } = storeToRefs(useUserStore());
 
 const alertMsg = computed(() => {
   if (user.value.enabled2Fa) {
     return "Vous avez activé la double authentification, votre compte est maintenant plus sécurisé"
   } else {
-    return "Vous n'avez pas activé la double authentification, votre compte est moins sécurisé, vous pouvez l'activer en cliquant sur le bouton ci-dessous"
+    return "Vous n'avez pas activé la double authentification, votre compte est moins sécurisé, vous pouvez l'activer en cliquant ici"
   }
 })
 </script>
