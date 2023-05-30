2e517a13a4972449af7a6e24cac268f25cd01b31
sieg.alexandre@gmail.com <sieg.alexandre@gmail.com>
2023-05-30 18:24:26 +0200

    refactor(header): show user avatar in header

--- components/Header.vue
+++ components/Header.vue
@@ -3,7 +3,26 @@
     <v-app-bar class="d-flex flex-row align-center justify-space-between">
       <v-app-bar-title>Smart Study</v-app-bar-title>
       <div class="d-flex flex-row align-center" v-if="user">
-        <NuxtLink to="/profile"><p class="mr-3">{{ user.email }}</p></NuxtLink>
+        <v-menu
+          open-on-hover
+          open-delay="0"
+        >
+          <template v-slot:activator="{ props }">
+            <div v-bind="props" class="d-flex flex-row align-center">
+              <v-avatar :image="userImage" class="mr-2"></v-avatar>
+              <p class="mr-3 user-email">{{ user.email }}</p>
+            </div>
+          </template>
+
+          <v-list>
+            <NuxtLink v-for="(item, index) in items" :key="index" :to="item.url">
+              <v-list-item>
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item>
+            </NuxtLink>
+          </v-list>
+        </v-menu>
+
         <v-btn class="mr-3" variant="tonal" @click="logout">Se déconnecter</v-btn>
       </div>
       <div v-else>
@@ -25,6 +44,19 @@ import { storeToRefs } from "pinia"
 const userStore = useUserStore()
 const { user } = storeToRefs(userStore)
 
+const userImage = computed(() => {
+  if (user.value?.profilePicture) {
+    return user.value.profilePicture.url
+  }
+  return "/default.jpg"
+})
+
+const items = ref([
+  { title: "Mon profil", url: "/profile" },
+  { title: "Mes cours", url: "/courses" },
+  { title: "Espace formateur", url: "/teacher"}
+])
+
 const logout = () => {
   useUserStore().setAccessToken('')
   window.location.reload()
@@ -32,6 +64,9 @@ const logout = () => {
 </script>
 
 <style lang="scss">
+.user-email {
+  cursor: default;
+}
 .v-app-bar {
   height: 60px;
   width: 100vw;
