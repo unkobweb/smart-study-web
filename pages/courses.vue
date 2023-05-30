8dfcb0466001a01fc3705037621926831eaf0a20
sieg.alexandre@gmail.com <sieg.alexandre@gmail.com>
2023-05-30 18:24:44 +0200

    feat(pages): create courses and teacher pages

---
+++ pages/courses.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="d-flex flex-column align-center justify-center">
+    <img src="/no-courses.jpg" alt="">
+    <h1>Vous n'avez acheté aucun cours</h1>
+    <NuxtLink to="/">
+      <v-btn class="btnPrimary mt-4">Allez sur la place de marché</v-btn>
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup>
+
+const courses = ref([])
+
+</script>
+
+<style lang="scss" scoped>
+img {
+  max-height: 40vh;
+  width: auto;
+  height: auto;
+}
+</style>
