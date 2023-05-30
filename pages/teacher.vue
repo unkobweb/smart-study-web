9412a4a32f144ab899332b4fe3517de385a6200f
sieg.alexandre@gmail.com <sieg.alexandre@gmail.com>
2023-05-30 18:27:10 +0200

    feat(teacher): add usefull info in script

--- pages/teacher.vue
+++ pages/teacher.vue
@@ -5,5 +5,14 @@
 </template>
 
 <script setup>
+import { storeToRefs } from "pinia";
+
+// permet de s'assurer que l'utilisateur est bien connecté quant il arrive sur cette page
+definePageMeta({
+  middleware: ["auth"]
+})
+
+// permet de récupérer les données de l'utilisateur qui sont stockées dans le store
+const { user } = storeToRefs(useUserStore());
 
 </script>
